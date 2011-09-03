628e21db6e490f7472fc49c814d2710ad0ec55ca
Yuki Nishijima <mail@yukinishijima.net>
2011-09-04 08:49:25 +0900

    added redis initialization.

--- spec/rightchoice_spec.rb
+++ spec/rightchoice_spec.rb
@@ -1,7 +1,27 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Rightchoice" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe Rightchoice do
+  before do
+    Rightchoice.redis.flushall
+  end
+
+  it "can set a namespace through a url-like string" do
+    Rightchoice.redis.should be_true
+    Rightchoice.redis.namespace.should == :resque
+    Rightchoice.redis = 'localhost:6379/namespace'
+    Rightchoice.redis.namespace.should == 'namespace'
+  end
+
+  it "works correctly with a Redis::Namespace param" do
+    new_redis = Redis.new(:host => "localhost", :port => 6379)
+    new_namespace = Redis::Namespace.new("namespace", :redis => new_redis)
+    Rightchoice.redis = new_namespace
+    Rightchoice.redis.should == new_namespace
+  end
+
+  it "works correctly with a Redis param" do
+    new_redis = Redis.new(:host => "localhost", :port => 6379)
+    Rightchoice.redis = new_redis
+    Rightchoice.redis.class.should == Redis::Namespace
   end
 end
