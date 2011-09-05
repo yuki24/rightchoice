4676b6861181b4b39a64cf628d776408202e3002
Yuki Nishijima <mail@yukinishijima.net>
2011-09-05 16:32:47 +0900

    require experiment model.

--- lib/rightchoice.rb
+++ lib/rightchoice.rb
@@ -1,6 +1,7 @@
 require 'redis/namespace'
 require 'rightchoice/dashboard'
 require 'rightchoice/models/track'
+require 'rightchoice/models/experiment'
 require 'rightchoice/models/alternative'
 require 'rightchoice/helpers/abtest_helper'
 
