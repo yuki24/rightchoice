f99d7ee5c43cf3e4958a845afa8cba7d627352e4
Yuki Nishijima <mail@yukinishijima.net>
2011-10-27 15:06:33 +0900

    added MultiVariation class

---
+++ lib/rightchoice/models/multi_variations.rb
@@ -0,0 +1,106 @@
+module Rightchoice
+  class MultiVariations
+    attr_accessor :multivariate_name, :variations
+
+    def initialize(multivariate_name, options={})
+      @multivariate_name = multivariate_name.to_s
+      @variations = []
+      @selections = {}
+      @participants_count = (options[:participants_count] || 0)
+      @votes_count = (options[:votes_count] || 0)
+    end
+
+    def name
+      multivariate_name
+    end
+
+    def selections
+      @selections.tap do |s|
+        variations.each {|v| s.store(v.name, v.choice) }
+      end
+    end
+
+    def expectation()
+    end
+
+    def dispersion
+    end
+
+    def redis_key
+      combinations = selections.to_a.map{|a| a.join(":") }.join(".")
+      "#{name}.#{combinations}"
+    end
+
+    def confident?
+    end
+
+    def save
+      if redis.exists?(redis_key)
+        redis.hmset(redis_key,
+                    :available => @available,
+                    :paricipants_count => @participants_count,
+                    :votes_count => @votes_count)
+      end
+    end
+
+    def vote!
+      redis.hincrby(redis_key, :votes_count, 1) if redis.exists?(redis_key)
+    end
+
+    def disable(testname, combinations)
+      redis.hmset(redis_key, :available, false) if redis.exists?(redis_key)
+    end
+
+    def destroy
+    end
+
+    def self.find_by_testname_and_key(testname, redis_key)
+      if exists?(testname, redis_key)
+        # get the values @available, @participants_count, @votes_count from redis
+
+        Rightchoice::MultiVariations
+          .new(:available => available,
+               :paricipants_count => participants_count,
+               :votes_count => votes_count,).tap do |v|
+#          v.
+        end
+      end
+    end
+
+    def self.available?(testname, combination)
+      if exists?(testname, combination)
+        Rightchoice.redis.hget("#{testname}.#{combination}", "available") == "true"
+      else
+        # return false or raise error?
+      end
+    end
+
+    def self.vote!(testname, combination)
+      if available?(testname, combination)
+        Rightchoice.redis.hincrby("#{testname}.#{combination}", :votes_count, 1)
+      else
+        # raise CombinationNotFound, "The combination doesn't exist."
+      end
+    end
+
+    def self.exists?(testname, combination)
+      Rightchoice.redis.exists?("#{testname}.#{combination}")
+    end
+
+    private
+
+    def redis
+      @_redis ||= Rightchoice.redis
+    end
+
+    def self.variations_by_selections!(selections)
+      # Variation.new(:variation_name2, "hoge", "fuga", :select => "hoge")
+    end
+
+    # MultiVariation.selections_by_key!("name.variation1:foo.variation2:hoge")
+    #  => {"variation1" => "foo", "variation2" => "hoge"}
+    def self.selections_by_key!(redis_key)
+      Hash[*redis_key.tr(".",":").split(":")[1..-1]]
+    end
+  end
+end
