16448e82f9fa4bf0468481231aeb3c25856ce9f0
Yuki Nishijima <mail@yukinishijima.net>
2011-11-07 17:05:33 +0900

    implemented list for variation.

--- lib/rightchoice/models/variation.rb
+++ lib/rightchoice/models/variation.rb
@@ -1,6 +1,7 @@
 module Rightchoice
   class Variation
     attr_accessor :name, :alternatives
+    attr_reader :child, :parent
 
     def initialize(variation_name, *alternatives)
       options = alternatives.pop if alternatives.last.is_a?(Hash)
@@ -14,6 +15,52 @@ module Rightchoice
 
     def choice
       @choice ||= random_select
+    end
+
+    def child=(variation)
+      # some validations should be add
+      # - variations can not have themeselves as a child
+      # - child can not be a parent
+      unless self.end?
+        @child = variation
+        variation.add_parent(self)
+        variation
+      end
+    end
+
+    def parent=(variation)
+      # some validations should be add
+      # - variations can not have themeselves as a parent
+      # - parent can not be a child
+      unless self.root?
+        @parent = variation
+        variation.add_child(self)
+        variation
+      end
+    end
+
+    def add_parent(variation)
+      @parent = variation
+    end
+
+    def add_child(variation)
+      @child = variation
+    end
+
+    def root!
+      @_root = true
+    end
+
+    def end!
+      @_end = true
+    end
+
+    def root?
+      @_root == true
+    end
+
+    def end?
+      @_end == true
     end
 
     private
--- spec/models/variation_spec.rb
+++ spec/models/variation_spec.rb
@@ -19,12 +19,40 @@ describe Rightchoice::Variation do
     context 'with option params' do
       subject { @valid_variation }
       its(:name) { should == "test_name" }
+      its(:alternatives) { should == ["foo", "bar"] }
       its(:choice) { should == "foo" }
     end
 
     context 'with invalid option params' do
       subject { @invalid_variation }
+      its(:alternatives) { should == ["foo", "bar"] }
       its(:choice) { should_not == "" }
+    end
+  end
+
+  describe "listing" do
+    before :all do
+      @v1 = Rightchoice::Variation.new(:name1, "foo", "bar")
+      @v2 = Rightchoice::Variation.new(:name2, "foo", "bar")
+      @v3 = Rightchoice::Variation.new(:name3, "foo", "bar")
+      @v4 = Rightchoice::Variation.new(:name4, "foo", "bar")
+    end
+
+    it "should set v2 as a child to v1" do
+      @v1.root!
+      @v1.child = @v2
+      @v2.child = @v3
+      @v3.child = @v4
+      @v4.end!
+
+      @v1.root?.should == true
+      @v1.child.should == @v2
+      @v2.parent.should == @v1
+      @v2.child.should == @v3
+      @v3.parent.should == @v2
+      @v2.child.should == @v3
+      @v3.parent.should == @v2
+      @v4.end?.should == true
     end
   end
 end
